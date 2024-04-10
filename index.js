e215811c785988c38b65cabcfaf35fbf981daed0
niaodtiantang <80442013+niaodtiantang@users.noreply.github.com>
2024-04-10 13:39:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-// 判断是否支持serviceWorker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js', {scope: './'}).then(function(registration) {
-      // 注册成功
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // 注册失败
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        console.log("正在安装 Service worker");
+      } else if (registration.waiting) {
+        console.log("已安装 Service worker installed");
+      } else if (registration.active) {
+        console.log("激活 Service worker");
+      }
+    } catch (error) {
+      console.error(`注册失败：${error}`);
+    }
+  }
+};
+
+// …
+
+registerServiceWorker();
